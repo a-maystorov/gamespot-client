2cba7596d318443701faa3ac669f00c40f2427a2
Alkin Maystorov <alkin971@gmail.com>
2022-06-06 19:55:46 +0200

    display games date in table format

--- src/components/GameList.tsx
+++ src/components/GameList.tsx
@@ -6,13 +6,26 @@ interface GameListProps {
 
 function GameList({ games }: GameListProps) {
   return (
-    <ul className="list-group">
-      {games.map((game) => (
-        <li key={game._id} className="list-group-item">
-          {game.title}
-        </li>
-      ))}
-    </ul>
+    <table className="table">
+      <thead className="table-dark">
+        <tr>
+          <th>Title</th>
+          <th>Genre</th>
+          <th>Stock</th>
+          <th>Rate</th>
+        </tr>
+      </thead>
+      <tbody>
+        {games.map((game) => (
+          <tr>
+            <td>{game.title}</td>
+            <td>{game.genre.name}</td>
+            <td>{game.numberInStock}</td>
+            <td>{game.dailyRentalRate}&euro;</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
 
